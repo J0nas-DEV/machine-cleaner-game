576507a3529b9517edd41df9a9f47f1ea8ab5356
J0nas-DEV <99317564+J0nas-DEV@users.noreply.github.com>
2026-04-25 00:59:47 +0800

    1
    1

---
+++ machine cleaner/assets/spawner.go
@@ -0,0 +1,16 @@
+components {
+  id: "spawner"
+  component: "/assets/spawner.script"
+}
+embedded_components {
+  id: "poopfactory"
+  type: "factory"
+  data: "prototype: \"/assets/poop/poop.go\"\n"
+  ""
+}
+embedded_components {
+  id: "gearsfactory"
+  type: "factory"
+  data: "prototype: \"/assets/gears/gears.go\"\n"
+  ""
+}
---
+++ machine cleaner/main/game_manager.go
@@ -0,0 +1,4 @@
+components {
+  id: "game_manager"
+  component: "/main/game_manager.script"
+}
